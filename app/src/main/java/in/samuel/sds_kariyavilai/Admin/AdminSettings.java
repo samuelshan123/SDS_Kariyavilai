ecb45abd4a0276c5dd0124096089723140b486c3
samuel  shan <samuelshan123@gmail.com>
2021-06-10 11:32:11 +0530

    s

--- app/src/main/java/in/samuel/sds_kariyavilai/Admin/AdminSettings.java
+++ app/src/main/java/in/samuel/sds_kariyavilai/Admin/AdminSettings.java
@@ -1,10 +1,14 @@
 package in.samuel.sds_kariyavilai.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import in.samuel.sds_kariyavilai.R;
 
@@ -30,6 +34,19 @@ public class AdminSettings extends AppCompatActivity {
 
     public void addadmin(View view) {
         startActivity(new Intent(getApplicationContext(), Admins.class));
+
+    }
+
+    public void notify(View view) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.sdzshn3.onesignalapi");
+        if (launchIntent != null) {
+            startActivity(launchIntent);
+        } else {
+
+            Toast.makeText(getApplicationContext(),"coudn't find notification package please install from playstore",Toast.LENGTH_LONG).show();
+            Intent i2=new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sdzshn3.onesignalapi&hl=en"));
+            startActivity(i2);
+        }
 
     }
 }
