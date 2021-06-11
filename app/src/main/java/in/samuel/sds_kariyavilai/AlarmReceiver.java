382a5e39b2d822fdfd483a2019fd666345caa8a0
samuel  shan <samuelshan123@gmail.com>
2021-06-11 15:37:52 +0530

    s

---
+++ app/src/main/java/in/samuel/sds_kariyavilai/AlarmReceiver.java
@@ -0,0 +1,58 @@
+package in.samuel.sds_kariyavilai;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.e("TAG", "Alarm received");
+        sendNotification(context, intent.getStringExtra("data"));
+    }
+
+    public static void sendNotification(Context mcontext, String messageBody) {
+        Intent intent = new Intent(mcontext, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(mcontext, R.string.default_notification_channel_id, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager notificationManager = (NotificationManager) mcontext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(mcontext.getString(R.string.default_notification_channel_id), "Rewards Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(new long[]{0, 500, 200, 500});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(mcontext, mcontext.getString(R.string.default_notification_channel_id))
+                .setContentTitle("இன்று நீங்கள் ஜெபித்தீர்களா?")
+                .setContentText("நீங்கள் இன்று வேதத்தை தியானித்தீர்களா?")
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+
+        notificationManager.notify(R.string.default_notification_channel_id, notificationBuilder.build());
+    }
+}
--- app/src/main/java/in/samuel/sds_kariyavilai/MainActivity.java
+++ app/src/main/java/in/samuel/sds_kariyavilai/MainActivity.java
@@ -1,13 +1,22 @@
 package in.samuel.sds_kariyavilai;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +24,15 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         finish();
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-checkInternetConenction();
+        checkInternetConenction();
+        scheduleAlarm();
+
         int SPLASH_TIME_OUT = 1000;
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -52,4 +65,42 @@ checkInternetConenction();
             Toast.makeText(this, "  Please connect to internet ", Toast.LENGTH_LONG).show();
         }
     }
+    @SuppressLint("ShortAlarm")
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void scheduleAlarm() {
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        alarmIntent.putExtra("data", "New Notification Recived");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, R.string.default_notification_channel_id, alarmIntent, PendingIntent.FLAG_ONE_SHOT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        Calendar now = Calendar.getInstance();
+
+        // alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),10000, pendingIntent);
+
+
+      /*long afterTwoMinutes = SystemClock.elapsedRealtime() + 10* 1000;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            alarmManager.setExactAndAllowWhileIdle
+                    (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            afterTwoMinutes, pendingIntent);
+        else
+            alarmManager.setExact
+                    (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            afterTwoMinutes, pendingIntent);
+*/
+
+        calendar.set(Calendar.HOUR_OF_DAY,8);
+        calendar.set(Calendar.MINUTE,00);
+        calendar.set(Calendar.SECOND,00);
+        if (now.after(calendar)) {
+            Log.d("Hey","Added a day");
+            calendar.add(Calendar.DATE, 1);
+        }
+
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+    }
+
 }
+
