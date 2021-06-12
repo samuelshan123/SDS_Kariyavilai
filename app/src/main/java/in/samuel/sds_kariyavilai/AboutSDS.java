5563cbefff3bd0b68d6cf94a8186e208949bab2e
samuel  shan <samuelshan123@gmail.com>
2021-06-12 14:01:29 +0530

    s

--- app/src/main/java/in/samuel/sds_kariyavilai/AboutSDS.java
+++ app/src/main/java/in/samuel/sds_kariyavilai/AboutSDS.java
@@ -2,7 +2,12 @@ package in.samuel.sds_kariyavilai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AboutSDS extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class AboutSDS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_sds);
+
+        CircleImageView circleImageView = (CircleImageView)findViewById(R.id.pastor);
+        ImageView fam =(ImageView)findViewById(R.id.pasfam);
+        ImageView men =(ImageView)findViewById(R.id.mens);
+        ImageView women =(ImageView)findViewById(R.id.women);
+        circleImageView.setImageResource(R.drawable.pastor);
+        fam.setImageResource(R.drawable.pasfam);
+        men.setImageResource(R.drawable.mens);
+        women.setImageResource(R.drawable.women);
+
+
+
     }
 }
