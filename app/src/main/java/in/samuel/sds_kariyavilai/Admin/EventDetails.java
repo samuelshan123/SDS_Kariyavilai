8be0b33c2ccd194f53626d20f8acab06dd24a74b
samuel  shan <samuelshan123@gmail.com>
2021-06-13 08:58:48 +0530

    s

--- app/src/main/java/in/samuel/sds_kariyavilai/Admin/EventDetails.java
+++ app/src/main/java/in/samuel/sds_kariyavilai/Admin/EventDetails.java
@@ -4,31 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Locale;
 
 import in.samuel.sds_kariyavilai.ChurchMembers.UserEvents;
 import in.samuel.sds_kariyavilai.R;
 
 public class EventDetails extends AppCompatActivity {
-
-    TextView tvtitle,tvdesc;
+TextToSpeech t1;
+    TextView tvtitle, tvdesc;
     int position;
+    FloatingActionButton button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
 
+
         //Initializing Views
         tvdesc = findViewById(R.id.eventdesc);
         tvtitle = findViewById(R.id.eventtit);
-
-        Intent intent =getIntent();
+        button =findViewById(R.id.speak);
+        Intent intent = getIntent();
         position = intent.getExtras().getInt("position");
 
         tvtitle.setText(UserEvents.eventDatasArrayList.get(position).getEventTitle());
         tvdesc.setText(UserEvents.eventDatasArrayList.get(position).getEventDescription());
 
 
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String input = tvdesc.getText().toString();
+                t1.speak(input, TextToSpeech.QUEUE_FLUSH, null);
+
+
+
+
+                /* if(input.contains("hi")){
+                    txttwo=(TextView) findViewById(R.id.bottxt);
+                    out = "i'm fine.How can i help u";
+                    txttwo.setText(out);
+                    t1.speak(out, TextToSpeech.QUEUE_FLUSH, null);
+                }
+                else {
+String err = "sorry i don't no";
+                    txttwo.setText(err);
+                    t1.speak(err, TextToSpeech.QUEUE_FLUSH, null);
+                }*/
+
+            }
+        });
+
+
     }
+
 }
