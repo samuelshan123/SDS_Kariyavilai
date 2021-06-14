d998d84c5c372b341f13d0b6db68982163cb526e
samuel  shan <samuelshan123@gmail.com>
2021-06-14 11:16:36 +0530

    s

--- app/src/main/java/in/samuel/sds_kariyavilai/Contacts.java
+++ app/src/main/java/in/samuel/sds_kariyavilai/Contacts.java
@@ -2,7 +2,10 @@ package in.samuel.sds_kariyavilai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contacts extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Contacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+    }
+
+    public void pascall(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:"+"9488272724"));
+        startActivity(callIntent);
+    }
+
+    public void praunty(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:"+"9488505097"));
+        startActivity(callIntent);
     }
 }
